5d892eed7a654a2d7ff6b26239adacb7dba176ba
afamusyafa <afamusyafa@gmail.com>
2018-01-07 18:35:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Flat desain : Adobe CS6</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/onepage-scroll.css" />
+        <link href='assets/fonts/Righteous.css' rel='stylesheet' type='text/css'>
+        <link href='assets/fonts/Voltaire.css' rel='stylesheet' type="text/css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head> 
+    <body>
+        <header>
+            <img src="assets/img/biore-logo1.jpg" alt="logo mini">
+            <h1><a href="#"> BIORE FACIAL FOAM</a></h1>
+            <h3></table> <a href="#"> Sabun Muka Berkualitas</a></h3>
+        </header>
+        <div class="main">
+            <section class="page one">
+                <div class="page-container">
+                    <h2><img src="assets/img/logo1.png" alt="logo mini"> Biore Facial Foam</h2>
+                    <p align="justify"> Sering cuci muka dengan facial foam atau sabun cuci muka? Membersihkan wajah secara teratur dengan Biore facial foam memang jadi cara yang paling praktis. Tapi apakah selama ini kita sudah menggunakannya dengan benar dan mengikuti cara yang tepat?
+                    <b></b>
+
+                    Agar bisa mendapat hasil wajah yang bersih maksimal tanpa menyebabkan iritasi, ada sejumlah tips dan langkah yang perlu diikuti dalam menggunakan facial foam. Yuk, kita ikuti langkah-langkahnya di sini, Tangan Harus dalam Keadaan Bersih, Gunakan Facial Foam Setelah Mandi, Bersihkan Wajah dengan Gerakan Memutar Perlahan, Awali dengan Air Hangat, Akhiri dengan Air Dingin,dan Keringkan dengan Handuk Lembut. Nah, itu tadi sejumlah hal penting yang perlu diperhatikan saat menggunakan Biore facial foam. Semoga bermanfaat, ladies!.
+
+                    Berikut ini
+                    </p>
+					<p> Review </p>
+                    <video width="500" controls>
+                    <source src="assets/vid/Biore Facial Foam.mp4" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+            
+            <section class="page two">
+                <div class="page-container">
+                    <h2>Biore Antibacterial Body Foam Bright Care 250ml</h2>
+                    <p align="justify">Biore dengan "Antibacterial + Healthy Beautiful Skin" tidak hanya bantu melindungi dari kuman tapi juga menjaga cerahnya kulit yang lembut. Memadukan Antibakteri (IPMP) dan Ekstrak Buah Kiwi dalam formula Skin Purifying Technology (SPT) memberikan perlindungan terhadap kuman yang melekat pada kotoran dan keringat serta menjaga cerahnya kulit dan sekaligus menjaga kelembaban alami kulit. </p><br>
+                    <p> Review </p>
+                     <video width="500" controls>
+                    <source src="assets/vid/Iklan Biore Pore & Oil Care - Facial Foam.MP4" type="video/mp4"/> 
+                    
+                </div>
+            </section>
+            
+            <section class="page three">
+                <div class="page-container">
+                    <h2>PRODUCT BIORE</h2>
+                    <p align="justify"> Biore dengan "Antibacterial + Healthy Beautiful Skin" tidak hanya bantu melindungi dari kuman tapi juga menjaga cerahnya kulit yang lembut. Memadukan Antibakteri (IPMP) dan Ekstrak Buah Kiwi dalam formula Skin Purifying Technology (SPT) memberikan perlindungan terhadap kuman yang melekat pada kotoran dan keringat serta menjaga cerahnya kulit dan sekaligus menjaga kelembaban alami kulit. </p><br>
+                    <p> Review </p><br>
+                    <h2><img src="assets/img/bio_ff.jpg"</h2>
+                     
+                    
+                </div>
+            </section>
+        </div>
+
+        <script type="text/javascript" src="assets/js/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.onepage-scroll.js"></script>
+        <script type="text/javascript">
+        $(".main").onepage_scroll({
+                sectionContainer: "section",
+                easting: "cubic-bezier(0.180, 0.900, 0.410, 1.210)"
+        });
+        </script>
+    </body>
+</html>
